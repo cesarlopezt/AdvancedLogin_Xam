61f02102d825412b3b835c2482a704d8a7daa865
cesarlopezt <cesarlopez2808@gmail.com>
2021-10-07 01:27:21 -0400

    Add Focus and Platform Specifics

---
+++ AdvancedLogin/AdvancedLogin/Effects/FocusEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AdvancedLogin.Effects
+{
+    class FocusEffect: RoutingEffect
+    {
+        public FocusEffect() : base("MyCompany.FocusEffect") { }
+    }
+}
